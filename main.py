5bf1c3d7344e731f6078f954d5ab7a0587acb447
Dan Dean <dpd5518@psu.edu>
2023-01-03 14:30:27 -0500

    added a db

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import sys
+import Card
+import sqlite3 as db
 
-def main():
-    for i in sys.argv:
-        print(i) 
-    return 0
+connection = db.connect("card.db") 
+cursor = connection.cursor()
+
+def init_db():
+  cursor.execute("""
+  CREATE TABLE cards(
+    front TEXT,
+    back TEXT,
+    created DATETIME,
+    last_reviewed DATETIME
+  )""")
+  connection.commit()
+  
 
 if __name__ == "__main__":
-    main()
+  init_db()
+
+connection.close()
