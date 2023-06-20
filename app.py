77745ac9b62cb161139e2c5f84b58030b3f359b6
Dan Dean <dandean@Dans-MacBook-Pro.local>
2023-06-20 08:29:23 -0400

    transfered code into app

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 import typer
 from rich import print
+from rich.console import Console
+from rich.table import Table
+from Cards import Card, Database
+con = Console()
 app = typer.Typer()
+db = Database()
 
 @app.command()
 def sayhello(name: str):
     print(f"hello {name}!")
 
+@app.command()
+def saygoodbye(name: str):
+    print(f"goodbye {name}!")
+
+@app.command()
+def new(front: str, back: str):
+    #TODO add optional
+    new_card = Card(front,back)
+    db.push_card(new_card)
+    print(f"added card {new_card}")
+
+def main():
+    cards = db.cards_from_db()
+    table = Table("Front", "Back","Box")
+    for card in cards:
+        table.add_row(card.get_front(),card.get_back(),str(card.get_current_bin()))
+    con.print(table)
 
 if __name__ == "__main__":
-     app()
-
+    app()
