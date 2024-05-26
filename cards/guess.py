95842b43fb518cb1eaeb40e0789f54b731d5541f
dandeandean <dpd5518@psu.edu>
2024-05-26 11:18:22 -0400

    rewrite begins

--- cards/guess.py
+++
@@ -1,6 +0,0 @@
-def exactly_correct(guess: str, answer: str) -> bool:
-    return guess.strip().lower() == answer.strip().lower()
-
-
-def approximately_correct(guess: str, answer: str) -> bool:
-    return guess.strip().lower() in answer.strip().lower()
